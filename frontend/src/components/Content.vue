7a26e6f152ffb27fbfa2210b905a7ba55bd41125
kostyaiscool <yanubtitrupzaeal@gmail.com>
2025-01-19 10:04:43 +0200

    *вставьте сюда безумно важную инфу*

--- frontend/src/components/Content.vue
+++ frontend/src/components/Content.vue
@@ -14,6 +14,10 @@ export default {
       videos: [
         { id: '1', title: 'Video 1', thumbnail: 'thumb1.jpg' },
         { id: '2', title: 'Video 2', thumbnail: 'thumb2.jpg' },
+        { id: '3', title: 'Video 3', thumbnail: 'thumb2.jpg' },
+        { id: '4', title: 'Video 4', thumbnail: 'thumb2.jpg' },
+        { id: '5', title: 'Video 5', thumbnail: 'thumb2.jpg' },
+        { id: '6', title: 'Video 6', thumbnail: 'thumb2.jpg' },
       ],
     };
   },
